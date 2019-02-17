625241f19c036fd6e8f572d2a566077ac00c47d2
Hong Baojin <13809735212@163.com>
2019-02-18 00:31:53 +0800

    write to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>
-    App
+  <div id="app">
+    <router-view></router-view>
+    <FooterGuide/>
   </div>
 </template>
 
 <script>
-  export default {}
+  import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+
+  export default {
+    components: {
+      FooterGuide
+    }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  #app
+    width 100%
+    height 100%
+    background #f5f5f5
 </style>
